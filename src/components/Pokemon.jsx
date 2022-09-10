354a125c7a6b3ce6dc2424ec41be838d64d5caee
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2022-09-10 21:50:07 +0200

    fixed 404 error  och user message on api search.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -9,6 +9,7 @@ import Image from "react-bootstrap/Image";
 const About = () => {
   const [pokemon, setPokemon] = useState([]);
   const [nameToSearch, setNameToSeach] = useState("");
+  const [searchOk, setSearchOk] = useState(true);
 
   useEffect(() => {
 
@@ -18,8 +19,15 @@ const About = () => {
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${nameToSearch}`
       );
-
+      console.log(response.ok)
+      setSearchOk(response.ok);
+   
+      console.log(searchOk)
+     
       const data = await response.json();
+
+
+
 
       const arr2 = data.abilities.map((arr) => arr.ability.name)//TODO *
 
@@ -37,10 +45,75 @@ const About = () => {
     fetchData();
   }, [nameToSearch]);
 
+
   return (
     <>
     <h1>Pokemon Info</h1>
-{nameToSearch === ""?(
+{nameToSearch === "" && searchOk ?(
+  <>
+    
+    <p>Here you can search the PokeApi database to get info on your favorite pokemon.<br/>
+    Enter the name of the pokemon you wish to look up. </p>
+      <MyForm 
+      placeholder = "Ex: Pikachu or Ditto"
+      setAnswer={setNameToSeach}></MyForm>
+      <br/>
+      
+    </>)
+    :
+    nameToSearch !== "" && searchOk ?
+    (<>
+    <p>Search another pokemon</p>
+      <MyForm 
+      placeholder = "Ex: Eevee or Ditto"
+      setAnswer={setNameToSeach}></MyForm>
+      <br/>
+  <h1 style={{ color: "pink" }}>{pokemon.name}!</h1>
+      <Image fluid src={pokemon.image}></Image>
+      <Table style={{ color: "white" }} bg="" responsive bordered size="sm">
+        <thead>
+          <tr>
+            <th>Height:</th>
+            <td>{pokemon.height}</td>
+          </tr>
+          <tr>
+            <th>Weight:</th>
+            <td>{pokemon.weight}</td>
+          </tr>
+          <tr>
+            <th>Type:</th>
+            <td>{pokemon.type}</td>
+          </tr>
+          <tr>
+            <th>Abilities:</th>
+            <td>{pokemon.abilities}</td>
+          </tr>
+          <tr>
+            <th>Experience:</th>
+            <td>{pokemon.base_experience}</td>
+          </tr>
+        </thead>
+      </Table>
+      </>)
+      :
+      (
+        <>
+        <p>No pokemon with that name came up? try again</p>
+      <MyForm 
+      placeholder = "Ex: Eevee or Ditto"
+      setAnswer={setNameToSeach}></MyForm>
+      <br/>
+      
+        </>
+
+      )
+    }
+    </>
+  )
+};
+
+export default About;/* 
+{nameToSearch === "" && searchOk ?(
   <>
     
     <p>Here you can search the PokeApi database to get info on your favorite pokemon.<br/>
@@ -80,9 +153,4 @@ const About = () => {
         </thead>
       </Table>
       </>)
-    }
-    </>
-  )
-};
-
-export default About;
+    } */
